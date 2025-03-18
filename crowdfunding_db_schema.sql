5f38a5c83b9a90b549d1a723c4f4727315249bf2
dagimg16 <dagimkiflyohannes@gmail.com>
2025-03-18 11:33:12 -0400

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,24 +1,24 @@
 
 CREATE TABLE category (
-    category_id VARCHAR(5) PRIMARY KEY,
+    category_id VARCHAR(4) PRIMARY KEY,
     category VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR(10) PRIMARY KEY,
+    subcategory_id VARCHAR(8) PRIMARY KEY,
     subcategory VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE contacts (
-    contact_id VARCHAR(10) PRIMARY KEY,
+    contact_id int PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL
 );
 
 CREATE TABLE campaign (
-    cf_id VARCHAR(10) PRIMARY KEY,
-    contact_id VARCHAR(10) NOT NULL,
+    cf_id int PRIMARY KEY,
+    contact_id int NOT NULL,
     company_name VARCHAR(100) NOT NULL,
     description VARCHAR(255),  
     goal Float NOT NULL ,
@@ -29,8 +29,8 @@ CREATE TABLE campaign (
     currency CHAR(3) NOT NULL,
     launch_date DATE NOT NULL,
     end_date DATE NOT NULL,
-    category_id VARCHAR(5) NOT NULL,
-    subcategory_id VARCHAR(10) NOT NULL,
+    category_id VARCHAR(4) NOT NULL,
+    subcategory_id VARCHAR(8) NOT NULL,
     FOREIGN KEY (contact_id) REFERENCES contacts(contact_id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id) ON DELETE CASCADE
